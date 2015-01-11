279ae8af84d5146550dbcb8b29d03f211e69d541
Gregory Haney <exalted1cobra@Compaq.Ubuntu>
2015-01-11 17:56:15 -0600

    changed model pin again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :style => { :medium => "300x300>", :thumb => "100x100>"},
-					  :storage => s3,
-					  :bucket => ENV{'pinteresting7000'}
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"}
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
